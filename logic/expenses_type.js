69348b1fd837ef3bd6870b553ed5ee5845c0185d
guevarch <107594143+guevarch@users.noreply.github.com>
2023-10-09 17:22:31 -0400

    Add

--- logic/expenses_type.js
+++ logic/expenses_type.js
@@ -51,26 +51,26 @@ document.addEventListener("DOMContentLoaded", function() {
         var labels = Object.keys(groupedData);
         var amounts = Object.values(groupedData);
 
-        // Generate pie chart
-        var ctx = document.getElementById("pieChart").getContext("2d");
-        new Chart(ctx, {
-          type: "pie",
-          data: {
-            labels: labels,
-            datasets: [{
-              label: "Total Amount Debited",
-              data: amounts,
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.6)",
-                "rgba(54, 162, 235, 0.6)",
-                "rgba(255, 206, 86, 0.6)",
-                "rgba(75, 192, 192, 0.6)",
-                "rgba(153, 102, 255, 0.6)"
-                // Add more colors as needed
-              ],
-            }],
-          },
-        });
+        // // Generate pie chart
+        // var ctx = document.getElementById("pieChart").getContext("2d");
+        // new Chart(ctx, {
+        //   type: "pie",
+        //   data: {
+        //     labels: labels,
+        //     datasets: [{
+        //       label: "Total Amount Debited",
+        //       data: amounts,
+        //       backgroundColor: [
+        //         "rgba(255, 99, 132, 0.6)",
+        //         "rgba(54, 162, 235, 0.6)",
+        //         "rgba(255, 206, 86, 0.6)",
+        //         "rgba(75, 192, 192, 0.6)",
+        //         "rgba(153, 102, 255, 0.6)"
+        //         // Add more colors as needed
+        //       ],
+        //     }],
+        //   },
+        // });
 
         // Populate table with filtered and sorted data
         var table = document.getElementById("data-table");
